b9c98b7a9e361d9bd6fd735ac76e374210b215e5
Abrarcodes <abrarkachari@gmail.com>
2024-06-30 16:01:43 +0530

    add the changes

--- pattern1.py
+++ pattern1.py
@@ -1,21 +1,11 @@
-#for printing 4x4 matrix asterisks
-for i in range(5):
-    for j in range(5):
+'''
+****
+****
+****
+****
+'''
+for i in range(1,5):
+    for j in range(1,5):
         print("*",end=" ")
     print()
     
-    
-'''x=int(input("enter the range"))
-y=int(input("enter the range"))
-for i in range(x):
-    for y in range(y):
-        print("*",end="  ")
-    print()
-    
-    
-    ****
-    ****
-    ****
-    ****
-    
-'''
